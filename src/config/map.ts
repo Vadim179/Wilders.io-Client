160a8e76fbdb085c148357e7b0046f9ece7b01ea
Vadim Vengriniuc <54371115+Vadim179@users.noreply.github.com>
2024-03-10 12:25:58 +0100

    Attack resources animation

--- src/config/map.ts
+++ src/config/map.ts
@@ -12,65 +12,64 @@ export const texturesWithShadows = [
 ];
 
 // TODO: Generate random rotations
-
-export const mapEntities = Object.freeze([
-  // Decorations
+export const mapDecorations = Object.freeze([
   {
     texture: Texture.LargeHill,
     x: 400,
     y: 100,
-    id: "h1",
+    id: "hill-1",
     order: TextureRenderingOrderEnum.LargeHill
   },
   {
     texture: Texture.Pebble,
     x: 200,
     y: 200,
-    id: "p1",
+    id: "pebble-1",
     order: TextureRenderingOrderEnum.Pebble
   },
   {
     texture: Texture.LargePinkFlower,
     x: 180,
     y: 300,
-    id: "f1",
+    id: "flower-1",
     order: TextureRenderingOrderEnum.LargePinkFlower
   },
   {
     texture: Texture.SmallWhiteFlower,
     x: 250,
     y: 400,
-    id: "f2",
+    id: "flower-2",
     order: TextureRenderingOrderEnum.SmallWhiteFlower
   },
   {
     texture: Texture.LargeWhiteFlower,
     x: 100,
     y: 450,
-    id: "f3",
+    id: "flower-3",
     order: TextureRenderingOrderEnum.LargeWhiteFlower
-  },
+  }
+]);
 
-  // Physical objects
+export const mapEntities = Object.freeze([
   {
     texture: Texture.LargeRock,
     x: 350,
     y: 50,
-    id: "r1",
+    id: "stone-1",
     order: TextureRenderingOrderEnum.LargeRock
   },
   {
     texture: Texture.LargeDarkOakTree,
     x: 500,
     y: 250,
-    id: "t1",
+    id: "tree-1",
     order: TextureRenderingOrderEnum.LargeDarkOakTree
   },
   {
     texture: Texture.LargeOakTree,
     x: 650,
     y: 150,
-    id: "t2",
+    id: "tree-2",
     order: TextureRenderingOrderEnum.LargeOakTree
   }
 ]);
